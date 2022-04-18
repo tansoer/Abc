29da8070e87867e6197f55cc57ea6a773eb9ce01
Tanel Sõerd <tasoer@ttu.ee>
2022-04-18 12:44:32 +0300

    Removed Leeds & Loinc from ProductDbInitializer

--- Infra/Products/Initializers/ProductDbInitializer.cs
+++ Infra/Products/Initializers/ProductDbInitializer.cs
@@ -1,13 +1,7 @@
-﻿using Abc.Infra.Products.Initializers.Leeds;
-using Abc.Infra.Products.Initializers.Loinc;
-
-namespace Abc.Infra.Products.Initializers {
+﻿namespace Abc.Infra.Products.Initializers {
 
     public class ProductDbInitializer :DbInitializer {
 
-        public static void Initialize(ProductDb db) {
-            LeedsProductsCatalogInitializer.Initialize(db);
-            LoincCatalogInitializer.Initialize(db);
-        }
+        public static void Initialize(ProductDb db) { }
     }
 }
--- Tests/Infra/Products/Initializers/ProductDbInitializerTests.cs
+++ Tests/Infra/Products/Initializers/ProductDbInitializerTests.cs
@@ -33,22 +33,13 @@ namespace Abc.Tests.Infra.Products.Initializers {
         }
         [TestMethod] public void InitializeTest() { }
         [TestMethod] public void ProductsTest() => areEqual(0, db.Products.Count());
-        [TestMethod] public void ProductTypesTest() => areEqual(4245, db.ProductTypes.Count());
         [TestMethod] public void FeaturesTest() => areEqual(0, db.Features.Count());
         [TestMethod] public void FeatureSpecsTest() => areEqual(0, db.FeatureSpecs.Count());
         [TestMethod] public void FeatureTypesTest() => areEqual(0, db.FeatureTypes.Count());
         [TestMethod] public void PossibleFeatureValuesTest() => areEqual(0, db.PossibleFeatureValues.Count());
         [TestMethod] public void BatchesTest() => areEqual(0, db.Batches.Count());
         [TestMethod] public void BatchCheckedByPartiesTest() => areEqual(0, db.BatchCheckedByParties.Count());
-        [TestMethod] public void CatalogedProductsTest() => areEqual(70, db.CatalogedProducts.Count());
-        [TestMethod] public void CatalogEntriesTest() => areEqual(14, db.CatalogEntries.Count());
-        [TestMethod] public void ProductCategoriesTest() => areEqual(10, db.ProductCategories.Count());
-        [TestMethod] public void ProductCatalogsTest() => areEqual(4, db.ProductCatalogs.Count());
-        [TestMethod] public void PackageComponentsTest() => areEqual(435, db.PackageComponents.Count());
         [TestMethod] public void PackageContentsTest() => areEqual(0, db.PackageContents.Count());
-        [TestMethod] public void ProductSetsTest() => areEqual(435, db.ProductSets.Count());
-        [TestMethod] public void ProductSetContentsTest() => areEqual(435, db.ProductSetContents.Count());
-        [TestMethod] public void ProductInclusionsTest() => areEqual(3631, db.ProductInclusions.Count());
         [TestMethod] public void ProductRelationshipTypesTest() => areEqual(0, db.ProductRelationshipTypes.Count());
         [TestMethod] public void ProductRelationshipsTest() => areEqual(0, db.ProductRelationships.Count());
         [TestMethod] public void PricesTest() => areEqual(0, db.Prices.Count());
